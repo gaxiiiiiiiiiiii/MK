04e57a3855f1ed2c71984eb5cbf613693417686b
gaxiiiiiiiiiiii <yethan.goes.asna@gmail.com>
2020-09-08 14:19:47 +0900

    sub_on

--- Order.v
+++ Order.v
@@ -1,13 +1,13 @@
 Require Export Maps.
 
 Definition Max R X x :=
-  ~ exists y, y ∈ X /\ <|x,y|> ∈ R.
+  x ∈ X /\ ~ exists y, y ∈ X /\ <|x,y|> ∈ R.
 
 Definition Min R X x :=
-  ~ exists y, y ∈ X /\ <|y,x|> ∈ R.
+  x ∈ X /\ ~ exists y, y ∈ X /\ <|y,x|> ∈ R.
 
 Definition WellFounded R A :=
-  forall X, X ⊆ A -> exists x, Min R X x.
+  forall X, X <> ∅ -> X ⊆ A -> exists x, Min R X x.
 
 Definition WellOrderd R A :=
     Total R A /\ WellFounded R A.
@@ -42,8 +42,9 @@ Proof.
     - apply (conj H).
       intros x y xX yX.
       apply (H3 x y (XA x xX) (XA y yX)).
-  + intros Y YX.
+  + intros Y Y0 YX.
     apply H0.
+    apply Y0.
     intros i iY.
     apply XA.
     apply YX.
@@ -120,10 +121,193 @@ Proof.
   + apply (sub_wellorderd InRel X).
     done.
     apply (H x xX).
+Qed.
+
+Theorem cap_on a b :
+    a ∈ ON -> b ∈ ON -> a ∩ b ∈ ON.
+Proof.
+  intros aON bON.  
+  rewrite on.  
+  apply on in aON.
+  apply on in bON.
+  induction aON.
+  induction bON.
+  split.
+  + clear H0 H2.
+    induction H as [a_ H].
+    induction H1 as [b_ H1].
+    split.
+    - by apply cap_set.
+    - intros x xab i ix.
+      apply cap.
+      apply cap in xab.
+      induction xab as [xa xb].
+      specialize (H x xa i ix).
+      specialize (H1 x xb i ix).
+      done.
+  + apply (sub_wellorderd InRel a H0).
+    intros i iab.
+    apply cap in iab.
+    by induction iab.
+Qed.
+
+Lemma not_empty X :
+    X <> ∅ <-> exists x, x ∈ X.
+Proof.
+  split => [| H].
+  + apply contrapositive.
+    rewrite <- allnot_notexists.
+    intro H.
+    apply DoubleNegative.
+    apply equal => i.
+    specialize (H i).
+    split => [iX | i0].
+    - case (H iX).
+    - assert (i_ : M i) by (by exists ∅).
+      case ((empty i i_) i0).
+  + induction H as [x xX].
+    intro X0.
+    assert (x_ : M x) by (by exists X).
+    rewrite X0 in xX.
+    apply ((empty x x_) xX).
+Qed.  
+
+Lemma diff_sub x y :
+    x // y = ∅ <-> x ⊆ y.
+Proof.
+  rewrite equal.
+  split => [H i ix |H i].
+  + specialize (H i).
+    induction H.
+    apply contrapositive in H.
+    move : H.
+    rewrite diff.
+    rewrite DeMorgan_notand.
+    intro H.
+    induction H.
+    - case (H ix).
+    - by rewrite <- DoubleNegative.
+    - apply empty.
+      by exists x.
+  + split => [ixy | i0].
+    - apply diff in ixy.
+      induction ixy.
+      case (H1 (H i H0)).
+    - assert (i_ : M i) by (by exists ∅).
+      case ((empty i i_) i0).
 Qed.    
 
 
+Theorem sub_on a b :
+  a ∈ ON -> b ∈ ON -> a ⊆ b <-> a ∈ b \/ a = b.
+Proof.
+  intros aON bON.
+  assert (aon : a ∈ ON) by done .
+  assert (a_ : M a) by (by exists ON).
+  apply on in aON.
+  apply on in bON.
+  inversion aON.
+  induction bON.  
+  split => [H3 | H3].
+  + case (ExcludedMiddle (a = b)) as [ab | notab].
+    - by apply or_intror.
+    - apply or_introl.
+      assert (~ b ⊆ a).
+      * intro ba.
+        apply notab.
+        apply equal => i.
+        split => [ia | ib].
+          by specialize (H3 i ia).
+          by specialize (ba i ib).      
+      * induction  (diff_sub b a) as [H5 _].
+        apply contrapositive in H5.
+        assert (b // a ⊆ b).
+          intros i iba.
+          apply diff in iba.
+          apply iba.
+        induction H2.
+        specialize (H7 (b//a) H5 H6).
+        induction H7 as [x].
+        induction H7.
+        move : H8.
+        rewrite <- allnot_notexists.      
+        intro H8.
+        apply diff in H7.
+        induction H7 as [xb notxa].        
+        induction H1.
+        specialize (H7 x xb).
+        assert (x //  a = ∅).
+
+          apply equal => i.
+          split => [ixa | i0].
+            apply diff in ixa.
+            induction ixa.
+            specialize (H8 i).
+            apply DeMorgan_notand in H8.
+            induction H8.
+            move : H8.
+            rewrite diff.
+            rewrite DeMorgan_notand.
+            intro H8.
+            induction H8.
+            case (H8 (H7 i H9)).
+            case (H8 H10).
+            apply inrel in H9.
+            case (H8 H9).
+            by exists x.
+            by exists b.
+            assert (i_ : M i) by (by exists ∅).
+            case ((empty i i_) i0).
+        apply diff_sub in H9 as H10.
+        case (ExcludedMiddle (a ⊆ x)) as [ax | notax].
+          assert (a = x).
+            apply equal => i.
+            split => [ia |ix].
+              by specialize (ax i ia).
+              by specialize (H10 i ix).
+            by subst a.
+
+            assert (a // x <> ∅).
+              intro.
+              apply diff_sub in H11.
+              case (notax H11).
+            apply not_empty in H11.
+            induction H11 as [y].
+            apply diff in H11.
+            induction H11.
+            specialize (H3 y H11).
+            induction H2.
+            induction H13.
+            induction H14.
+            specialize (H15 x y xb H3).
+            move : H15.
+            repeat rewrite inrel.
+            intro.
+
+            induction H15 as [xy | H15].
+              induction H.
+              specialize (H15 y H11).
+              specialize (H15 x xy).
+              case (notxa H15).
+              induction H15 as [yx | xy].
+                case (H12 yx).
+                subst y.
+                case (notxa H11).
+            by exists b.
+            by exists b.
+            by exists b.
+            by exists b.
+            done.
+  + intros i ia.
+    induction H3 as [ab | ab].
+    - induction H1.
+      by specialize (H3 a ab i ia).
+    - by subst b.
+Qed.      
 
 
-
+    
+    
+              
+            
 
